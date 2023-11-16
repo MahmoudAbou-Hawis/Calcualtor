12374b2fafba5004d7ab553fd2f2d0e6f49fab04
Mahmoud Abou-Hawis <mahmoud.abohawis10@gmail.com>
2023-11-16 21:21:54 +0200

    mod function and header gard added

---
+++ Calculator/calculator.c
@@ -0,0 +1,6 @@
+#include "calculator.h"
+
+int mod(int num ,int  md)
+{
+    return ((num%md)+mod)%mod;
+}
---
+++ Calculator/calculator.h
@@ -0,0 +1,7 @@
+#ifndef CALCULATOR_H_
+#define CACULATOR_H_
+
+int mod(int num , int md);
+
+
+#endif
