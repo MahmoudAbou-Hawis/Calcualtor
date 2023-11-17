01651197a46ad9b0c3ea74784efe57b563211147
Yasmin <109252157+yasminEzF@users.noreply.github.com>
2023-11-17 07:55:12 +0200

    division feature (#4)

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,6 +1,10 @@
 #include "calculator.h"
 
-int mod(int num ,int  md)
+float div(int num1 , int num2)
 {
-    return ((num%md)+mod)%mod;
+	float result = 0;
+	if(num2 != 0){
+		result = (float)num1 / num2;
+	}
+    return result;
 }
